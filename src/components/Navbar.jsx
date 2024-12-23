a0085d94fe19403f2db8fbee1bac18668cd0d0c2
SgrAhmet <ahmet1aydos@gmail.com>
2024-12-23 13:14:00 +0300

    muiStyleFormArea

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,80 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Box,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from "@mui/material";
+import {
+  Menu,
+  Home,
+  Receipt,
+  People,
+  Report,
+  Settings,
+  Delete,
+} from "@mui/icons-material";
 
-const Navbar = () => {
+import logo from "../imgs/logo2.png"
+
+const Navbar = ({toggleDrawer}) => {
+
+    // console.log(toggleDrawer)
+    // toggleDrawer()
   return (
     <>
-    
-    
-    </>
-  )
-}
+      <Box
+        sx={{
+          flexGrow: 1,
+        //   width: "100%",
+          height:"10%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        //   border: "1px solid orange",
+        //   margin: "5px",
+        }}
+      >
+        <AppBar
+          position="static"
+          sx={{
+            // width: "80%",
+            bgcolor:"#FFFFFF",
+            padding:"10px"
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 ,color:" rgb(28, 57, 96)"}}
+              onClick={toggleDrawer(true)}
+            >
+              <Menu sx={{fontSize:"32px"}}/>
+            </IconButton>
+            {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Kabinari
+            </Typography> */}
+            <Box sx={{
+                // border:"1px solid black",
+                width:"15%",
+                height:"70%",
+                margin:"5px"
+            }}>
+            <img style={{height:"100%",width:"100%"}} src={logo} alt="Kabinari" srcset="" />
 
-export default Navbar
+            </Box>
+
+            {/* <Button color="inherit">Login</Button> */}
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/Style1.jsx
+++ src/pages/Style1.jsx
@@ -1,14 +1,37 @@
 import React, { useState } from "react";
-import { ThemeProvider, createTheme, Button, Box,List,ListItem,ListItemButton,ListItemText,ListItemIcon,Divider, ToggleButton, TextField,Typography,Drawer  } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
-import CheckIcon from '@mui/icons-material/Check';
-import { Home, Receipt, People, Report, Settings,Delete } from "@mui/icons-material";
+import {
+  ThemeProvider,
+  createTheme,
+  Button,
+  Box,
+  Divider,
+  ToggleButton,
+  TextField,
+  Typography,
+  Drawer,
+  Paper,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CheckIcon from "@mui/icons-material/Check";
+import {
+  Person,
+  CalendarMonth,
+  Assessment,
+  People,
+  Report,
+  Settings,
+  Delete,
+} from "@mui/icons-material";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import SideBar from "../components/SideBar";
-
+import Navbar from "../components/Navbar";
 
 const Style1 = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState(false);
 
   const theme = createTheme({
     palette: {
@@ -19,26 +42,190 @@ const Style1 = () => {
     },
   });
 
-
-
-
-//  SideBar===============================
+  //  SideBar===============================
   const [open, setOpen] = React.useState(false);
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
   };
-//  SideBar===============================
+  //  SideBar===============================
 
   return (
     <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          // border:"2px solid tomato",
+          height: "100vh",
+          // width:"100vw"
+        }}
+      >
+        <Navbar toggleDrawer={toggleDrawer} />
+        <SideBar open={open} toggleDrawer={toggleDrawer} />
+        <Box
+          sx={{
+            backgroundColor: "#DEDDDB",
+            // height: "90%",
+          }}
+        >
+          <Box
+            sx={{
+              // border:"1px solid red",
+              display: "flex",
+              padding: "10px",
+              // justifyContent:"space-around"
+            }}
+          >
+            <Typography
+              color="#A19D95"
+              fontSize={32}
+              fontFamily={"unset"}
+              marginLeft={"30px"}
+              marginY={"10px"}
+            >
+              Yeni Proje
+            </Typography>
+          </Box>
 
-      <Box sx={{
-        border:"5px solid tomato",
-        height:"100vh"
-      }}>
+          <Box
+            sx={{
+              border: "1px solid #FFFFFF",
+              bgcolor: "#FFFFFF",
+              borderRadius: "20px",
+              height: "80vh",
+              margin: "10px",
+              marginX: "30px",
+              boxShadow: 1,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                margin: "30px",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <Person sx={{ fontSize: "48px", color: "#A19D95" }} />
+                <Typography
+                  color="#A19D95"
+                  fontSize={24}
+                  // sx={{justifyContent:"center"}}
+                  display={"flex"}
+                  alignItems={"center"}
+                  // fontFamily={"unset"}
+                  // marginLeft={"30px"}
+                  marginX={"10px"}
+                >
+                  Müşteri İsmi
+                </Typography>
+              </Box>
 
-      <Button onClick={toggleDrawer(true)}><Settings /></Button>
-      <SideBar open={open} toggleDrawer={toggleDrawer}/>
+              <TextField
+                sx={{ width: "60%" }}
+                id="outlined-basic"
+                variant="outlined"
+              />
+              <Button
+                sx={{ backgroundColor: "#665B59", height: "80%" }}
+                variant="contained"
+              >
+                Kaydet
+              </Button>
+            </Box>
+
+            <Divider
+              sx={{ borderBottomWidth: 3, backgroundColor: "#DEDDDB" }}
+            />
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                margin: "30px",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <Assessment sx={{ fontSize: "48px", color: "#A19D95" }} />
+                <Typography
+                  color="#A19D95"
+                  fontSize={24}
+                  // sx={{justifyContent:"center"}}
+                  display={"flex"}
+                  alignItems={"center"}
+                  // fontFamily={"unset"}
+                  // marginLeft={"30px"}
+                  marginX={"10px"}
+                >
+                  İş Numarası
+                </Typography>
+              </Box>
+
+              <TextField
+                sx={{ width: "60%" }}
+                id="outlined-basic"
+                variant="outlined"
+              />
+
+              <Box height={"50px"} width={"95px"}></Box>
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                margin: "30px",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <CalendarMonth sx={{ fontSize: "48px", color: "#A19D95" }} />
+                <Typography
+                  color="#A19D95"
+                  fontSize={24}
+                  // sx={{justifyContent:"center"}}
+                  display={"flex"}
+                  alignItems={"center"}
+                  // fontFamily={"unset"}
+                  // marginLeft={"30px"}
+                  marginX={"10px"}
+                >
+                  Oluşturma Tarihi
+                </Typography>
+              </Box>
+
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  sx={{ width: "60%" }}
+                  label="Oluşturma Tarihi"
+                  // value={value}
+                  // onChange={(newValue) => setValue(newValue)}
+                />
+              </LocalizationProvider>
+
+              <Box height={"50px"} width={"140px"}></Box>
+            </Box>
+
+            <Divider
+              sx={{ borderBottomWidth: 3, backgroundColor: "#DEDDDB" }}
+            />
+          </Box>
+        </Box>
       </Box>
     </ThemeProvider>
   );
